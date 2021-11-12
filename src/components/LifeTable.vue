d9e67c903bd15b5b2c0975bc81998534d21849cb
Abylay Bazarbayev <abazarbayev@1cb.kz>
2021-11-12 18:31:58 +0600

    table render optimize

--- src/components/LifeTable.vue
+++ src/components/LifeTable.vue
@@ -1,24 +1,34 @@
 <template>
-    <table class="life-table"
-           :class="{'life-table__disable': !isSizeMatches}"
-           :border="tableBorder"
-           @mousedown="mouseDown"
-           @mouseup="mouseUp"
-           @mouseleave="mouseUp"
-    >
+<!--    <table class="life-table"-->
+<!--           :class="{'life-table__disable': !isSizeMatches}"-->
+<!--           :border="tableBorder"-->
+<!--           @mousedown="mouseDown"-->
+<!--           @mouseup="mouseUp"-->
+<!--           @mouseleave="mouseUp"-->
+<!--    >-->
 
-        <tr v-for="(rowData, i) in data" :key="i">
-            <td
-                v-for="(isActive, j) in rowData"
-                :key="j"
-                class="life-table__cell"
-                :class="{'life-table__active': isActive}"
-                @mouseover="overCell(i, j)"
-                @mousedown="selectCell([i, j])"
-            />
-        </tr>
+<!--        <tr v-for="(rowData, i) in data" :key="i">-->
+<!--            <td-->
+<!--                v-for="(isActive, j) in rowData"-->
+<!--                :key="j"-->
+<!--                class="life-table__cell"-->
+<!--                :class="{'life-table__active': isActive}"-->
+<!--                @mouseover="overCell(i, j)"-->
+<!--                @mousedown="selectCell([i, j])"-->
+<!--            />-->
+<!--        </tr>-->
 
-    </table>
+<!--    </table>-->
+  <div class="life-table" :class="{'life-table--show-border': showGrid}" @mousedown="mouseDown" @mouseup="mouseUp" @mouseleave="mouseUp">
+    <div class="life-table__row" v-for="(rowData, i) in data" :key="i">
+      <div class="life-table__cell"
+           v-for="(isActive, j) in rowData" :key="j"
+           :class="{'life-table__active': isActive}"
+           @mouseover="overCell(i, j)"
+           @mousedown="selectCell([i, j])"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -67,9 +77,28 @@ export default {
       height: 100%;
       border-spacing: 0;
       cursor: pointer !important;
+      display: grid;
+      grid-auto-rows: auto;
+
+      &__row {
+        height: auto;
+        display: flex;
+        flex-direction: row;
+        justify-content: stretch;
+      }
+
+      &__cell {
+        height: inherit;
+        width: 100%;
+        //border: 1px solid black;
+      }
 
         &__cell:hover {
           background-color: #d4d4d4;
+        }
+
+        &--show-border {
+          .life-table__cell { border: 1px solid black}
         }
 
         &__disable {
@@ -79,6 +108,8 @@ export default {
 
         &__active {
             background-color: black;
+          &:hover {background-color: #595959
+          }
         }
     }
 </style>
